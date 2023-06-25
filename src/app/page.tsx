ded08c800d47b265f1d02aebfdab4ece4561131a
psst54 <psst5491@naver.com>
2023-06-26 06:37:31 +0900

    ➕ setting styled-components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+"use client";
+
+import styled from "styled-components";
+
+const Tmp = styled.div`
+  width: 2rem;
+  height: 2rem;
+  background: black;
+`;
+
 export default function Home() {
-  return (
-    <main>
-      <div>test</div>
-    </main>
-  );
+  return <Tmp />;
 }
